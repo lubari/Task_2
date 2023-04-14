68a854f3c1b108f968c8b8b33f4d039f616784e0
Ana <analubari@gmail.com>
2023-04-14 04:17:36 -0300

    Task 2

--- Program.cs
+++ Program.cs
@@ -14,17 +14,35 @@ namespace Task_2
             string sentence = Console.ReadLine().ToLower();
             string[] words = ReplaceHyphens(sentence).Split();
             Console.WriteLine("Your word are: ");
-            PrintWords(words);
+            PrintArray(words);
             ArePalindromes(words);
             Console.ReadKey();
+            //
+
+            //Task_2
+            bool validArray = false;
+            while (!validArray)
+            {
+                int[] numbers = GetNumbers();
+                if(numbers != null)
+                {
+                    SwapArray(numbers);
+                    validArray = true;
+                }
+      
+            }
+            Console.ReadKey();
+            ///
+
         }
 
         static string ReplaceHyphens(string senteces)
-        {   
+        {
             string onlyLetters = "";
             foreach (char letter in senteces)
             {
-                if (char.IsLetter(letter) || ' '.Equals(letter)){
+                if (char.IsLetter(letter) || ' '.Equals(letter))
+                {
                     onlyLetters += letter;
                 }
             }
@@ -47,19 +65,85 @@ namespace Task_2
 
         static void ArePalindromes(string[] words)
         {
-            foreach(string word in words)
+            foreach (string word in words)
             {
                 IsPalindrome(word);
             }
         }
 
-        static void PrintWords(string[] words)
+        static void PrintArray(object[] array)
         {
-            foreach (string word in words)
+            foreach (string element in array)
             {
-                Console.WriteLine($"{word}");
+                Console.WriteLine($"{element}");
             }
         }
 
+        static int[] GetNumbers()
+        {
+            Console.WriteLine("Enter your numbers separeted with a coma: ");
+            string numbers = Console.ReadLine();
+            return MakeIntArray(numbers);
+        }
+
+        static int[] MakeIntArray(string numbers)
+        {
+            string[] stringsNumbers = numbers.Split(',');
+            int[] intArray = new int[stringsNumbers.Length];
+            int num;
+            int i = 0;
+
+            foreach(string number in stringsNumbers)
+            {
+                if (!int.TryParse(number, out num))
+                {
+                    return null;
+                }
+                intArray[i] = int.Parse(number);
+                i++;
+            }
+
+            return intArray;
+        }
+
+        static string InputNumber()
+        {
+            Console.WriteLine("Enter a number: ");
+            string input = Console.ReadLine();
+            bool isValidInput = false;
+            int num;
+
+            while (!isValidInput)
+            {
+                if (int.TryParse(input, out num))
+                {
+                    // The input is a valid number
+                    isValidInput = true;
+                }
+                else
+                {
+                    // The input is not a valid number, so we cannot cast it to int
+                    Console.WriteLine("The input is not a number.");
+                    Console.WriteLine("Enter a number ");
+                    input = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("The input value is: " + input);
+            return input;
+        }
+
+        static void SwapArray(int[] array)
+        { // Swap the elements using a loop
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                // Swap the i-th and (len(arr)-i-1)-th elements
+                int temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
+
+            // Print the swapped array
+            Console.WriteLine("Swapped array: [" + string.Join(", ", array) + "]");
+        }
     }
 }
